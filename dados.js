cd8e67f5c226903f88b0b1f1eabbb62d97b15602
Otávio Augusto <163211965+7otavio@users.noreply.github.com>
2024-09-09 23:34:13 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [ {
+    titulo: "Rayssa Leal",
+    descricao: "Rayssa Leal, conhecida como Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela se tornou a mais jovem medalhista olímpica da história do Brasil, trazendo o skate para o centro das atenções e inspirando uma nova geração de atletas",
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags: "skate fada sk8"
+},   
+{
+    titulo: "Beatriz Souza",
+    descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nos Jogos Olímpicos de Paris 2024 na categoria +78kg, sendo um dos destaques do judô brasileiro.",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+    tags: "judo judô judoka"
+},
+{
+    titulo: "Rebeca Andrade",
+    descricao: "Rebeca Andrade é uma ginasta artística brasileira que conquistou diversas medalhas em competições internacionais, incluindo o ouro no individual geral nos Jogos Olímpicos de Tóquio 2020.",
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    tags:"ginástica ginastica ouro"
+}
+];
+
+
